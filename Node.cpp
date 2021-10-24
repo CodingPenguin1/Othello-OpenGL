c3b3a9231e4990cc7c4fe5e86d0679df1605ba09
Ryan Joseph Slater <ryan.j.slater.2@gmail.com>
2021-10-24 15:37:47 -0400

    Added tree structure and move checking

--- Node.cpp
+++ Node.cpp
@@ -3,15 +3,123 @@
 #include <stdlib.h>
 #include <string.h>
 
-Node::Node(const uint16_t *&board) {
+Node::Node(const uint16_t *board) {
     memcpy(this->board, board, sizeof(uint16_t) * 8);
     this->n_children = 0;
-    this->children = NULL;
+    this->children = new Node *[0];
     this->score = 0;
     this->branch_score = 0.0f;
 }
 
 Node::~Node() {
-    if (this->children != NULL)
-        delete[] this->children;
+    delete[] this->children;
+}
+
+
+void Node::print_board() {
+    for (uint8_t row = 0; row < 8; ++row) {
+        for (uint8_t col = 0; col < 8; ++col) {
+            uint16_t current_space = (this->board[row] >> 2 * (8 - col - 1)) & 0x3;  // Shift to the right so that current cell is right most, 0 padded on left
+            switch (current_space) {
+                case 0:  // Empty
+                    printf(".");
+                    break;
+                case 1:  // Black
+                    printf("X");
+                    break;
+                case 2:  // White
+                    printf("O");
+                    break;
+                default:  // Error
+                    printf("?");
+                    break;
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+void Node::add_child(Node *child) {
+    this->n_children++;
+    Node **temp_children_array = new Node *[this->n_children];
+    for (uint8_t i = 0; i < this->n_children - 1; ++i)
+        temp_children_array[i] = this->children[i];
+    temp_children_array[this->n_children - 1] = child;
+    delete[] this->children;
+    this->children = temp_children_array;
+}
+
+
+void Node::create_next_states(uint8_t player) {
+    // Check all spaces for potential moves
+    for (uint8_t row = 0; row < 8; ++row) {
+        for (uint8_t col = 0; col < 8; ++col) {
+            if (this->check_valid_move(row, col, player)) {
+                // Create a new child with the move applied
+                Node *child = new Node(this->board);
+                child->board[row] += (player << (2 * (8 - col - 1)));
+                this->add_child(child);
+            }
+        }
+    }
+}
+
+
+bool Node::check_valid_move(uint8_t row, uint8_t col, uint8_t player) {
+    uint8_t **unpacked_board = this->get_unpacked_board();
+
+    // If space is not empty
+    if (unpacked_board[row][col] != 0)
+        return false;
+
+    // Iterate through each direction
+    for (int d_row = -1; d_row < 2; ++d_row) {
+        for (int d_col = -1; d_col < 2; ++d_col) {
+            int cur_row = row;
+            int cur_col = col;
+            // If direction is not 0,0
+            if (d_row != 0 || d_col != 0) {
+                // Iterate in that direction
+                cur_row += d_row;
+                cur_col += d_col;
+
+                bool found_enemy = false;
+                bool found_self = false;
+
+                while (cur_row > -1 && cur_row < 8 && cur_col > -1 && cur_col < 8) {
+                    if (unpacked_board[cur_row][cur_col] == player)
+                        found_self = true;
+                    if (unpacked_board[cur_row][cur_col] > 0 && unpacked_board[cur_row][cur_col] != player)
+                        found_enemy = true;
+
+                    // If found self before enemy, not valid move
+                    if (found_self && !found_enemy)
+                        break;
+
+                    // If gap found, not valid move
+                    if (unpacked_board[cur_row][cur_col] == 0)
+                        break;
+
+                    if (found_enemy && found_self)
+                        return true;
+
+                    cur_row += d_row;
+                    cur_col += d_col;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
+uint8_t **Node::get_unpacked_board() {
+    uint8_t **unpacked_board = new uint8_t *[8];
+    for (int row = 0; row < 8; ++row) {
+        unpacked_board[row] = new uint8_t[8];
+        for (uint8_t col = 0; col < 8; ++col)
+            unpacked_board[row][col] = (this->board[row] >> 2 * (8 - col - 1)) & 0x3;
+    }
+    return unpacked_board;
 }
--- main.cpp
+++ main.cpp
@@ -3,37 +3,33 @@
 #include <stdio.h>
 
 
-void print_board(uint16_t *board) {
-    for (uint8_t row = 0; row < 8; ++row) {
-        for (uint8_t col = 0; col < 8; ++col) {
-            uint16_t current_space = (board[row] >> 2 * (8 - col - 1)) & 0x3;  // Shift to the right so that current cell is right most, 0 padded on left
-            switch (current_space) {
-                case 0:  // Empty
-                    printf(".");
-                    break;
-                case 1:  // Black
-                    printf("X");
-                    break;
-                case 2:  // White
-                    printf("O");
-                    break;
-                default:  // Error
-                    printf("?");
-                    break;
-            }
-        }
-        printf("\n");
-    }
-}
-
-
 // int main(int argc, char **argv) {
 int main() {
     uint16_t *initial_board = new uint16_t[8];
     initial_board[3] = 0x0240;  // 1 is black and 2 is white
     initial_board[4] = 0x0180;
 
-    print_board(initial_board);
+    // print_board(initial_board);
+
+    Node *root = new Node(initial_board);
+    // root->create_next_states(1);
+    // printf("%d\n", root->n_children);
+    // root->print_board();
+    // for (int i = 0; i < root->n_children; i++) {
+    //     printf("\n");
+    //     root->children[i]->print_board();
+    // }
+
+    root->create_next_states(1);
+    printf("%d children\n", root->n_children);
+    printf("Original\n");
+    root->print_board();
+
+    for (int i = 0; i < root->n_children; i++) {
+        printf("\nChild %d\n", i);
+        root->children[i]->print_board();
+    }
+
 
     return 0;
 }
